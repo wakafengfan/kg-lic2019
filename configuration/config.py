861ab62dea3e7288e5e76569f26db3ffc0fcce01
冯璠 <fengfan@zhongan.io>
2019-05-28 14:59:58 +0800

    first

---
+++ configuration/config.py
@@ -0,0 +1,9 @@
+import os
+
+
+ROOT_PATH = os.path.normpath(os.path.join(os.path.abspath(os.path.dirname(__file__)), ".."))
+
+# data
+data_dir = os.path.join(ROOT_PATH, "data")
+model_dir = os.path.join(ROOT_PATH, "model")
+
